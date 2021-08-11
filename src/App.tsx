213b811cc52b039b26061d3aa8e0d44e2c5f8884
fernanda-oshiro <fernanda.oshiro@iguanafix.com>
2021-08-11 19:40:12 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
@@ -9,6 +9,7 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+
 
 interface GenreResponseProps {
   id: number;
@@ -51,9 +52,9 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  },[])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,6 @@
 import { MovieCard } from "./MovieCard";
+import { AutoSizer, Collection, CollectionCellRenderer, CollectionCellSizeAndPositionGetter} from 'react-virtualized'
+
 
 interface ContentProps {
   selectedGenre: {
@@ -20,18 +22,49 @@ interface ContentProps {
 }
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+  
+  const cellRenderer: CollectionCellRenderer = ({index, key, style}) => {
+    return (
+      <div key={key} style={style}>
+        <MovieCard
+          key={movies[index].imdbID}
+          title={movies[index].Title}
+          poster={movies[index].Poster}
+          runtime={movies[index].Runtime}
+          rating={movies[index].Ratings[0].Value} 
+        />
+      </div>
+    )
+  }
+
+  const cellSizeAndPositionGetter: CollectionCellSizeAndPositionGetter = ({index}) => {
+
+    return {
+      height: 400,
+      width: 200,
+      x: Math.floor(index%3) * 300,
+      y: Math.floor(index/3) * 400,
+    };
+  }
+  
   return (
     <div className="container">
       <header>
         <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
       </header>
 
-      <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
+      <main style={{height:'90vh', overflowY:'hidden'}}>
+        <AutoSizer>
+          {({height, width}) => (
+            <Collection
+              cellCount={movies.length}
+              cellRenderer={cellRenderer}
+              cellSizeAndPositionGetter={cellSizeAndPositionGetter}
+              height={height}
+              width={width}
+            />
+          )}
+        </AutoSizer>
       </main>
     </div>
   )
